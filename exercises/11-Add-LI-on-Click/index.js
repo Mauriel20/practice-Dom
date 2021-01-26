778912f79f5ded6f43cec3b35dde2945021760aa
Mauriel <josueazofeifajosue@gmail.com>
2021-01-26 23:30:26 +0000

    My-wor77k

--- exercises/11-Add-LI-on-Click/index.js
+++ exercises/11-Add-LI-on-Click/index.js
@@ -1,5 +1,7 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-
+    var addli = document.createElement('li');
+    var myul = document.getElementById('mylist');
+    addli.appendChild(myul.childNodes[3].);
 });
---
+++ exercises/12-Dynamic-HTML-String/index.js
@@ -0,0 +1,5 @@
+let mystring = "<p>Hello!</p> <strong>My friend</strong>";
+document.write(mystring);
+var fecha = new Date();
+var ano = fecha.getFullYear();
+document.write("we are in the year: " + ano);
